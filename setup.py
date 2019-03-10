5a75037b7c9848e85b857bc2e85d7c901dd8e3e3
Aaron Lichtman <aaronlichtman@gmail.com>
2019-03-10 08:44:54 -0400

    Add setup.py skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+# This script is used for the installation and removal of the rootkit
+
+def install():
+    pass
+
+
+def removal():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
