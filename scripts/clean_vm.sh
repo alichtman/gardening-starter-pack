7bfb82505d285a491a23dfb3d2c05aab73387471
Aaron Lichtman <aaronlichtman@gmail.com>
2019-04-05 16:15:06 -0400

    Fixed script

--- scripts/clean_vm.sh
+++ scripts/clean_vm.sh
@@ -3,5 +3,4 @@
 # This script cleans up the logs that garden writes obscene amounts of data to.
 # Basically, stops your VM from killing itself. Before I wrote this, my barebones
 # VM was taking up over 250GB.
-
-echo "osboxes.org" | sudo -S su -c "echo \"Hi\"; cat /dev/null > /var/log/kern.log*; cat /dev/null > /var/log/syslog*"
+sudo sh -c "echo \"Clearing logs.\"; cat /dev/null &> /var/log/kern.log; cat /dev/null &> /var/log/syslog"
