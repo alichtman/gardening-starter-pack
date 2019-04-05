b68658f649c595f4212f612162d4b6ca257f3835
Aaron Lichtman <aaronlichtman@gmail.com>
2019-04-05 12:32:34 -0500

    Fix clean script?

--- scripts/clean_vm.sh
+++ scripts/clean_vm.sh
@@ -3,10 +3,5 @@
 # This script cleans up the logs that garden writes obscene amounts of data to.
 # Basically, stops your VM from killing itself. Before I wrote this, my barebones
 # VM was taking up over 250GB.
-if [ "$EUID" -ne 0 ]
-	then echo "You need root privileges."
-	exit
-fi
 
-cat /dev/null > /var/log/kern.log*
-cat /dev/null > /var/log/syslog*
+echo "osboxes.org" | sudo -S su -c "echo \"Hi\"; cat /dev/null > /var/log/kern.log*; cat /dev/null > /var/log/syslog*"
